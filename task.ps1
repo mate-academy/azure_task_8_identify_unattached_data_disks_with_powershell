d51e3c8dbc59cc8a568e3de742cb6b5b4fd2acb8
alexk <alexkochubey13@gmail.com>
2024-07-20 12:18:29 +0300

    Solution?

--- task.ps1
+++ task.ps1
@@ -1,1 +1,15 @@
-# Write your code here
+$VirtualMachine = Get-AzVM `
+    -ResourceGroupName "mate-resources-ukwest" `
+    -Name "mate-vm"
+Remove-AzVMDataDisk `
+    -VM $VirtualMachine `
+    -Name "mate-task-2-disk"
+Update-AzVM `
+    -ResourceGroupName "mate-resources-ukwest" `
+    -VM $VirtualMachine
+
+$allDisks = Get-AzDisk -ResourceGroupName "mate-resources-ukwest"
+
+$unattachedDisks = $allDisks | Where-Object { $_.ManagedBy -eq $null -or $_.DiskState -eq 'Unattached' }
+
+$unattachedDisks | Select-Object -Property Id, Name, DiskSizeGB, DiskState | ConvertTo-Json | Set-Content -Path "./result.json"
