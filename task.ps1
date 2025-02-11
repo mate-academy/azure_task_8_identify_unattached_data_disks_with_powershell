2af05276f7eac30e2df8334a93aa2ae494980b9d
L1mbxbx <ilya.msd2003@gmail.com>
2025-02-11 18:32:58 +0200

    Solution

--- task.ps1
+++ task.ps1
@@ -1,1 +1,23 @@
-# Write your code here
+$resourceGroup = "mate-azure-task-5"
+$disks = Get-AzDisk -ResourceGroupName $resourceGroup
+$unattachedDisks = @()
+
+foreach ($disk in $disks) {
+    if (-not $disk.ManagedBy) {
+        $diskInfo = @{
+            Name = $disk.Name
+            DiskSizeGB = $disk.DiskSizeGB
+            Location = $disk.Location
+            SKU = $disk.Sku
+            OsType = $disk.OsType
+            CreationTime = $disk.TimeCreated
+            DiskState = $disk.DiskState
+        }
+
+        $unattachedDisks += $diskInfo
+    }
+}
+
+$unattachedDisks | ConvertTo-Json | Set-Content -Path ".\result.json"
+
+Write-Host "Unattached disks information has been saved to 'result.json'"
