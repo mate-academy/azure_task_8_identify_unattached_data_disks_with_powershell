e2c8fd02388c6480b64366eb3ca778a9d53f6990
Mykyta Kovalenko <mykyta.kovalenko0804@gmail.com>
2025-08-06 12:28:03 +0300

    fix the issue

--- task.ps1
+++ task.ps1
@@ -2,6 +2,6 @@ $resourceGroup = "mate-azure-task-5"
 
 $disks = Get-AzDisk -ResourceGroupName $resourceGroup
 
-$unattachedDisks = $disks | Where-Object{ $_.DiskState -eq "Unattached" -or $_.ManagedBy -eq $null}
+$unattachedDisks = $disks | Where-Object { ($_.DiskState -eq "Unattached" -or $_.ManagedBy -eq $null) -and !$_.OsType }
 
 $unattachedDisks | ConvertTo-Json | Set-Content -Path "./result.json"
