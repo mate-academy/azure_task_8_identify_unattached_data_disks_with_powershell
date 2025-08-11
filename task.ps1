533f521797d2520ac8616305db9d262df43a2dde
Illia Bilous <krabbys1111@gmail.com>
2025-08-11 23:58:02 +0300

    Add script to filter and export unattached Azure disks to JSON

--- task.ps1
+++ task.ps1
@@ -1,1 +1,13 @@
-# Write your code here
+# Вказуємо назву ресурсної групи
+$resourceGroup = "mate-azure-task-5"
+
+# Отримуємо всі диски з цієї групи
+$allDisks = Get-AzDisk -ResourceGroupName $resourceGroup
+
+# Фільтруємо unattached диски (ManagedBy пустий або null)
+$unattachedDisks = $allDisks | Where-Object {
+    -not $_.ManagedBy  # якщо ManagedBy пустий або null
+}
+
+# Записуємо інформацію у JSON у файл result.json
+$unattachedDisks | ConvertTo-Json | Set-Content -Path "./result.json"
