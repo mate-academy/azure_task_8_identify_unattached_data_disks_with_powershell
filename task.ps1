29295dad97c845cffb682c76ab486aa779ed1c1e
Danylo Melnychuk <danyakid2005@gmail.com>
2025-10-13 14:30:23 +0300

    Add -Force to Out-File and check OsType for null in task.ps1

--- task.ps1
+++ task.ps1
@@ -1,7 +1,7 @@
 $disks = Get-AzDisk -ResourceGroupName "mate-azure-task-5"
 
 $unattachedDisks = $disks | Where-Object {
-  ($_.ManagedBy -eq $null -or $_.ManagedBy -eq "") -or ($_.DiskState -eq "Unattached")
+  (($_.ManagedBy -eq $null -or $_.ManagedBy -eq "") -or ($_.DiskState -ieq "Unattached")) -and ($_.OsType -eq $null)
 }
 
-$unattachedDisks | ConvertTo-Json -Depth 4 | Out-File -FilePath "./result.json" -Encoding utf8
+$unattachedDisks | ConvertTo-Json -Depth 4 | Out-File -FilePath "./result.json" -Encoding utf8 -Force
