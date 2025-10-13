e103b60765a1007ae3b84ddeda7f97f6431a1636
Danylo Melnychuk <danyakid2005@gmail.com>
2025-10-13 14:40:29 +0300

    Handle empty unattached disks and update artifacts.json

--- task.ps1
+++ task.ps1
@@ -4,4 +4,8 @@ $unattachedDisks = $disks | Where-Object {
   (($_.ManagedBy -eq $null -or $_.ManagedBy -eq "") -or ($_.DiskState -ieq "Unattached")) -and ($_.OsType -eq $null)
 }
 
+if (-not $unattachedDisks) {
+  $unattachedDisks = @()
+}
+
 $unattachedDisks | ConvertTo-Json -Depth 4 | Out-File -FilePath "./result.json" -Encoding utf8 -Force
