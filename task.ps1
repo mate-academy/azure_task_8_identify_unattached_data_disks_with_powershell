b61cc5545ed30c43e627d585115bbe830499e1d6
Yurii <yurij.kenu@gmail.com>
2025-10-30 19:03:13 +0100

    disks: found disks

--- task.ps1
+++ task.ps1
@@ -1,1 +1,2 @@
-# Write your code here
+$disks = Get-AzDisk -ResourceGroupName "mate-azure-task-5" | Where-Object { $_.ManagedBy -eq $null }
+$disks | ConvertTo-Json | Out-File -FilePath "result.json"
