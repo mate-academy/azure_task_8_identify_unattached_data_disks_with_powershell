8dd83ee5e3d0f4c1197fab787e0bb2aa05b28b9e
Tim <tymur.prokhorov@asquad.io>
2025-12-20 21:09:27 +0200

    identify unattached data disks via pwsh script

--- task.ps1
+++ task.ps1
@@ -1,1 +1,16 @@
-# Write your code here
+$resourceGroupName = "mate-azure-task-5"
+$disks = Get-AzDisk -ResourceGroupName $resourceGroupName
+
+$unattachedDisks = $disks | Where-Object {
+    $_.DiskState -eq "Unattached" -or $_.ManagedBy -eq $null
+}
+
+$result = $unattachedDisks | Select-Object `
+    Name,
+    Location,
+    DiskSizeGB,
+    Sku,
+    DiskState,
+    ManagedBy
+
+$result | ConvertTo-Json -Depth 5 | Set-Content "result.json"
