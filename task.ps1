439fb019d48bc302f0a8cd64eba612b37f683313
Vladyslav Kozlov <kozlov.vlad@lll.kpi.ua>
2026-01-18 16:14:12 +0200

    update PowerShell script and add JSON output for unattached disks

--- task.ps1
+++ task.ps1
@@ -1,1 +1,4 @@
-# Write your code here
+$unattachedDisks = Get-AzDisk -ResourceGroupName "mate-azure-task-2" |
+    Where-Object { $_.DiskState -eq "Unattached" -or $_.ManagedBy -eq $null }
+
+$unattachedDisks | ConvertTo-Json | Out-File -FilePath "result.json" -Encoding utf8
