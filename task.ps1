3177022e0f90280c53691563f875e2c5c49cb9eb
Vladyslav Kozlov <kozlov.vlad@lll.kpi.ua>
2026-01-18 16:23:11 +0200

    Fixed the resource group

--- task.ps1
+++ task.ps1
@@ -1,4 +1,4 @@
-$unattachedDisks = Get-AzDisk -ResourceGroupName "mate-azure-task-2" |
+$unattachedDisks = Get-AzDisk -ResourceGroupName "mate-azure-task-5" |
     Where-Object { $_.DiskState -eq "Unattached" -or $_.ManagedBy -eq $null }
 
 $unattachedDisks | ConvertTo-Json | Out-File -FilePath "result.json" -Encoding utf8
