8aedcf2bfba2a1c4cd54860db1dfb422471c40d1
Никита <Goldbergnik1993@gmail.com>
2026-03-01 18:04:18 +0100

    dev

--- task.ps1
+++ task.ps1
@@ -1,11 +1,9 @@
 $rg = "mate-azure-task-5"
 $output = "result.json"
 
-# Получаем диски и фильтруем отсоединенные
 $unattachedDisks = Get-AzDisk -ResourceGroupName $rg | Where-Object { $_.DiskState -eq "Unattached" }
 
-# Используем @(), чтобы результат всегда был массивом в JSON
 @($unattachedDisks) | ConvertTo-Json -Depth 10 | Out-File -FilePath $output -Encoding utf8
 
-Write-Host "Найдено дисков: $(@($unattachedDisks).Count). Результат сохранен в массив в $output"
+Write-Host "Done. Found $(@($unattachedDisks).Count) disks."
 
