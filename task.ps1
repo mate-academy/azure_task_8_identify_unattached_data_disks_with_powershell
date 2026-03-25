7ed862b73befe80c2aebb504dc37dd2fbbd0f07b
Vasiliy Tongobash <v.tonhobash@gmail.com>
2026-03-25 11:52:20 +0200

    Solution

--- task.ps1
+++ task.ps1
@@ -1,1 +1,1 @@
-# Write your code here
+$diskfind = Get-AzDisk -ResourceGroupName "mate-azure-task-5" | Where-Object { $_.DiskState -eq "Unattached" } | ConvertTo-Json -Depth 100 | Out-File .\result.json
