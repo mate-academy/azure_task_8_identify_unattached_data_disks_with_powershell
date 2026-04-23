0dbe7cdada3b2f39b295074e8950bacb0dbe36ce
Владислав Устьянцев <vladisloveust@gmail.com>
2026-04-23 20:15:49 +0300

    Solution1

--- task.ps1
+++ task.ps1
@@ -1,1 +1,7 @@
-# Write your code here
+$results = New-Object System.Collections.Generic.List[PSObject]
+
+$AzureDisksOffline = Get-AzDisk -ResourceGroupName "mate-azure-task-5" | Where-Object {$_.DiskState -eq "Unattached"}
+
+$results.Add($AzureDisksOffline)
+
+$results | ConvertTo-Json -Depth 5 | Out-File -FilePath ".\result.json" -Encoding UTF8
