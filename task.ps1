a58cbb2659440b88a4febe45b0b6c79f8f932777
NazarKulyk6 <nazarkulyk6@gmail.com>
2026-04-29 15:44:14 +0200

    Update artifacts and result files; add PowerShell script to retrieve unattached disks

--- task.ps1
+++ task.ps1
@@ -1,1 +1,7 @@
-# Write your code here
+$resourceGroupName = "mate-azure-task-5"
+$resultPath = Join-Path $PSScriptRoot "result.json"
+
+$disks = Get-AzDisk -ResourceGroupName $resourceGroupName
+$unattachedDisks = $disks | Where-Object { $_.DiskState -eq "Unattached" }
+
+$unattachedDisks | ConvertTo-Json -Depth 10 | Out-File -FilePath $resultPath -Force
