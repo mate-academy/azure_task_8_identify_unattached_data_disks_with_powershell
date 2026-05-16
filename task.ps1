87ebb903f41d95100867df5a8daf99dfa811388f
Yuriy Mishchuk <yuramishchuk1703@gmail.com>
2026-05-16 23:35:09 +0300

    remove dick

--- task.ps1
+++ task.ps1
@@ -1,1 +1,4 @@
 # Write your code here
+$disks = Get-AzDisk -ResourceGroupName "mate-azure-task-5"
+$unattached = $disks | Where-Object { $_.DiskState -eq "Unattached" }
+$unattached | Select-Object Name, DiskSizeGB, DiskState | ConvertTo-Json | Set-Content -Path ./result.json
