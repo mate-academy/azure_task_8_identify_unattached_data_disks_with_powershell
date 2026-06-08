37ad87e007f481b9dfaf4b4cb643f45346fcf39f
Oleg <oleggavelya@gmail.com>
2026-06-08 17:34:58 +0300

    add remove DataDisk command in scripts task

--- task.ps1
+++ task.ps1
@@ -1,1 +1,4 @@
-Get-AzDisk -ResourceGroupName "MATE-AZURE-TASK-5" | Where-Object {$_.DiskState -eq "Unattached" } | ConvertTo-Json | Out-File -Path "result.json"
+$VirtualMachine = Get-AzVM -ResourceGroupName "mate-azure-task-5" -Name "azure-task-2"
+Remove-AzVMDataDisk -VM $VirtualMachine -Name "azure-task-2"
+Update-AzVM -ResourceGroupName "mate-azure-task-5" -VM $VirtualMachine
+Get-AzDisk -ResourceGroupName "mate-azure-task-5" | Where-Object {$_.DiskState -eq "Unattached" } | ConvertTo-Json | Out-File -Path "result.json"
