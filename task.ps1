4d8a08f4f5ee9e6248dcb0884c2ae5dbb5bc90c1
Oleh K <hmkl.public@gmail.com>
2026-08-10 12:00:30 +0200

    Solution

--- task.ps1
+++ task.ps1
@@ -1,1 +1,7 @@
-# Write your code here
+$resultFile = "./result.json"
+
+$unattachedDisks = Get-AzDisk | Where-Object { $_.DiskState -eq "Unattached"}
+
+if ($unattachedDisks.Count -gt 0) {
+    $unattachedDisks | ConvertTo-Json | Out-File -FilePath $resultFile
+}
