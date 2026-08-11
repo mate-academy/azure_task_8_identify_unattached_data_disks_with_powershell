24c1b966d41d4fc9efc36720663c124fb97e1ec8
nikol <77391893+nikol-reukr@users.noreply.github.com>
2026-08-11 23:27:13 +0300

    update task.ps1 to output unattached disks as an array

--- task.ps1
+++ task.ps1
@@ -2,4 +2,6 @@ $allDisks = Get-AzDisk -ResourceGroupName "mate-azure-task-5"
 
 $unattachedDisks = $allDisks | Where-Object { $_.DiskState -eq "Unattached" }
 
-$unattachedDisks | ConvertTo-Json -Depth 10 > result.json
+$jsonOutput = ConvertTo-Json -InputObject @($unattachedDisks) -Depth 10
+
+Out-File -InputObject $jsonOutput -FilePath "result.json" -Encoding utf8
