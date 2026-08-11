4d9ce064595bbde93234e9a7f87d76ce6c961802
nikol <77391893+nikol-reukr@users.noreply.github.com>
2026-08-11 23:01:25 +0300

    Added pwsh script for finding unattached disks in resource group

--- task.ps1
+++ task.ps1
@@ -1,1 +1,5 @@
-# Write your code here
+$allDisks = Get-AzDisk -ResourceGroupName "mate-azure-task-5"
+
+$unattachedDisks = $allDisks | Where-Object { $_.DiskState -eq "Unattached" }
+
+$unattachedDisks | ConvertTo-Json -Depth 10 > result.json
