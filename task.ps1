e652116d985c8e81809e8d6f74cbc4de10a50028
Alexandr <nook17n1@gmail.com>
2026-09-03 09:51:44 +0300

    Solution3

--- task.ps1
+++ task.ps1
@@ -5,25 +5,33 @@
 # Open /etc/fstab
 # Remove artical about our disk
 # sudo umount /dev/sdc1 /data
+# Check /proc/mounts disk should be unmounted
 # Write your code here
 
 #Command for PowerShell
-$VirtualMachine = Get-AzVM `
-  -ResourceGroupName "mate-azure-task-5" `
-  -Name "demo-vm"
-Remove-AzVMDataDisk `
-  -VM $VirtualMachine `
-  -Name "demo-disk"
-Update-AzVM `
-  -ResourceGroupName "mate-azure-task-5" `
-  -VM $VirtualMachine
 
-$disks = Get-AzDisk -ResourceGroupName 'mate-azure-task-5'
-$result = @()
+try {
+    $VirtualMachine = Get-AzVM -ResourceGroupName "mate-azure-task-5" -Name "demo-vm"
+    Remove-AzVMDataDisk -VM $VirtualMachine -Name "demo-disk"
+    Update-AzVM -ResourceGroupName "mate-azure-task-5" -VM $VirtualMachine
 
-foreach ($disk in $disks) {
-    if ($disk.DiskState -eq "Unattached") {
-        $result += $disk
+    do {
+        $disk = Get-AzDisk -ResourceGroupName "mate-azure-task-5" -Name "demo-disk"
+        if ($disk.DiskState -ne "Unattached") {
+            Start-Sleep -Seconds 5
+        }
+    } while ($disk.DiskState -ne "Unattached")
+    
+
+    $disks = Get-AzDisk -ResourceGroupName 'mate-azure-task-5'
+    $result = @()
+    foreach ($disk in $disks) {
+        if ($disk.DiskState -eq "Unattached") {
+            $result += $disk
+        }
     }
+    $result | ConvertTo-Json | Out-File -Path ./result.json
 }
-$result | ConvertTo-Json | Out-File -Path ./result.json
+catch {
+    Write-Host "Error: $_"
+}
