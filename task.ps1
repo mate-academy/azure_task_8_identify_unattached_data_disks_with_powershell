cfdb7000fee6bbbb59de7faf867d48f184ef08ed
Diana Khmeliuk <dianakhmeluyk@gmail.com>
2026-09-03 11:16:53 +0300

    solution

--- task.ps1
+++ task.ps1
@@ -1,5 +1,12 @@
 # Write your code here
 $rgName = "mate-azure-task-5"
+$vmName   = "task5-vm"
+$diskName = "task5-datadisk"
+
+$vm = Get-AzVM -ResourceGroupName $rgName -Name $vmName
+Remove-AzVMDataDisk -VM $vm -Name $diskName
+Update-AzVM -ResourceGroupName $rgName -VM $vm
+
 $disks = Get-AzDisk -ResourceGroupName $rgName
 
 $unattachedDisks = $disks | Where-Object {$_.DiskState -eq "Unattached" }
