7bfd10535ee4afec44ed3f7ee6c12fbf13805349
Alexandr <nook17n1@gmail.com>
2026-09-03 09:37:11 +0300

    Solution1

--- task.ps1
+++ task.ps1
@@ -1,4 +1,23 @@
+#command for SSH
+# ssh nook17@10.0.1.4
+# dmesg | grep SCSI
+# sudo -i blkid
+# Open /etc/fstab
+# Remove artical about our disk
+# sudo umount /dev/sdc1 /data
 # Write your code here
+
+#Command for PowerShell
+$VirtualMachine = Get-AzVM `
+  -ResourceGroupName "mate-azure-task-5" `
+  -Name "demo-vm"
+Remove-AzVMDataDisk `
+  -VM $VirtualMachine `
+  -Name "demo-disk"
+Update-AzVM `
+  -ResourceGroupName "mate-azure-task-5" `
+  -VM $VirtualMachine
+
 $disks = Get-AzDisk -ResourceGroupName 'mate-azure-task-5'
 $result = @()
 
